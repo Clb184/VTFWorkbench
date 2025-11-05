5255de59bc386b088ca9539ec6d7c3b762da907d
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-11-05 00:19:00 -0500

    Basic window setup

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,10 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
 
 	// Create window
-	GLFWwindow* window = glfwCreateWindow(512, 512, "test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "test", NULL, NULL);
 	if(nullptr == window) { std::cout << "Failed creating window\n"; return -1; }
 
 	glfwMakeContextCurrent(window);
@@ -27,27 +28,54 @@ int main() {
 	ImGuiIO& io = ImGui::GetIO();
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 	ImGui::StyleColorsDark();
+	// Initialize ImGUI for OpenGL
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 330");
-	glViewport(0, 0, 512, 512);
+
+	// Configure viewport
+	glViewport(0, 0, 800, 600);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
+	char buf[256] = " ";
 
 	while(!glfwWindowShouldClose(window)){
+		// Process messages
 		glfwPollEvents();
+
+		// New ImGUI frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 		
-		ImGui::Begin("Test");
+		// Window itself
+		ImGui::SetNextWindowPos({0.0f, 0.0f});
+		ImGui::SetNextWindowSize({800.0f, 600.0f});
+		ImGui::Begin("vtftool_root", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar);
+		
+		// Test some groupings
+		ImGui::BeginGroup();
+		//ImGui::Text("texture");
+	       	//ImGui::SameLine();
+		ImGui::InputText("texture", buf, 256);
+		ImGui::EndGroup();
+
+		//ImGui::BeginGroup();
+		//ImGui::EndGroup();
+		
+		// End main window
 		ImGui::End();
 
+		// Render contents and draw
 		ImGui::Render();
-
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Move the Swap Chain
 		glfwSwapBuffers(window);
 	}
 
+	// Terminate all
+	glfwDestroyWindow(window);
+	window = nullptr;
 	std::cout << "Ending program...\n";
 	glfwTerminate();
 }
